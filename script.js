ed96edd3d785766251f6b3151aef513161c3e3ee
olympiawoj <owojcik7@gmail.com>
2020-09-26 08:48:42 -0400

    Letter Press Event Handler

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const words = ["application", "programming", "interface", "wizard"];
 
 let selectedWord = words[Math.floor(Math.random() * words.length)];
 
-const correctedLetters = [];
+const correctLetters = [];
 const wrongLetters = [];
 
 // Show hidden word
@@ -22,7 +22,7 @@ function displayWord() {
   wordEl.innerHTML = `
         ${selectedWord.split("").map((letter) => `
             <span class="letter">
-                ${correctedLetters.includes(letter) ? letter : ''}
+                ${correctLetters.includes(letter) ? letter : ''}
             </span>
         `).join('')}
     `;
@@ -32,5 +32,46 @@ function displayWord() {
         popup.style.display = 'flex';
     }
 }
+
+// Update the wrong letters
+function updateWrongLettersEl(){
+    console.log('Update wrong')
+}
+
+// Show notification
+function showNotification(){
+    notification.classList.add('show');
+
+    setTimeout(()=>{
+        notification.classList.remove('show')
+    }, 2000) // 2000 milliseconds, 2 seconds
+}
+
+
+// Keydown letter press
+window.addEventListener('keydown', e=>{
+    // console.log(e.keyCode) // each key has a key code 
+    if(e.keyCode>=65 && e.keyCode <= 90){ // A through Z
+        const letter = e.key;
+
+        if(selectedWord.includes(letter)){
+            if(!correctLetters.includes(letter)){
+                correctLetters.push(letter)
+
+                displayWord()
+            } else {
+                showNotification();
+            }
+        } else {
+            if(!wrongLetters.includes(letter)){
+                wrongLetters.push(letter);
+
+                updateWrongLettersEl()
+            } else {
+                showNotification();
+            }
+        }
+    }
+})
 
 displayWord();
